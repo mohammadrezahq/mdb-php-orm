a5895c76d9d461a5150c49b07ea93ec3d6ca345a
mohammadreza Haghshenas <mohammadrezah.official@gmail.com>
2021-11-12 18:39:02 +0330

    udate composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e2588d0fa7d82dacdcaf0efd16f7894a25d5a91b',
+        'name' => 'mor/mdb',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mor/mdb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e2588d0fa7d82dacdcaf0efd16f7894a25d5a91b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
